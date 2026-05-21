e2038353af9244038948316e32b8335d3792fa68
Marcelo Ordoñez <camarord2002@gmail.com>
2026-05-20 19:44:12 -0500

    Ajusta API y recursos de Passenger

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const hostname = "0.0.0.0";
 // Este archivo lo envuelve para que Passenger lo pueda levantar.
 process.env.HOSTNAME = hostname;
 process.env.PORT = String(port);
+process.env.UV_THREADPOOL_SIZE = process.env.UV_THREADPOOL_SIZE || "2";
+process.env.NEXT_PRIVATE_WORKER_THREADS = process.env.NEXT_PRIVATE_WORKER_THREADS || "false";
 
 // Carga el server que Next.js generó en el build standalone
 require("./.next/standalone/server.js");
